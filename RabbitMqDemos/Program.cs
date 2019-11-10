57047d94a6fc575dfce1a71ea3bc0fb53928df26
mustafa sarıel <mustafasariel@hotmail.com>
2019-11-10 12:48:20 +0300

    Add project files.

---
+++ RabbitMqDemos/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Mesajınız :");
+            string strMessage = Console.ReadLine();
+            Person person = new Person() { Name = "mustafa", SurName = "sarıel", ID = 1, BirthDate = new DateTime(1981, 2, 4), Message = strMessage };
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                //channel.QueueDeclare(queue: "msariel2", durable:    false, exclusive: false,autoDelete: false, arguments: null);
+              //  channel.QueueDeclare("msariel4", false, false, false, null);
+                string message = JsonConvert.SerializeObject(person);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "msariel2",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($"Gönderilen kişi: {person.Name}-{person.SurName}");
+            }
+
+            Console.WriteLine(" İlgili kişi gönderildi...");
+            Console.ReadLine();
+        }
+
+    }
+
+
+}
---
+++ RabbitMq_Engine/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMq_Engin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMq_Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                //channel.QueueDeclare(queue: "msariel2",
+                //                     durable: false,
+                //                     exclusive: false,
+                //                     autoDelete: false,
+                //                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Person person = JsonConvert.DeserializeObject<Person>(message);
+                    Console.WriteLine($" Adı: {person.Name} Soyadı:{person.SurName} [{person.Message}]");
+                };
+                channel.BasicConsume(queue: "msariel2",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+               
+                Console.ReadLine();
+            }
+        }
+    }
+}
